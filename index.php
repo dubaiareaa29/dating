4d05180f879adb96a548c2167806bb989ff75a3a
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-01-19 02:58:17 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+    <title>Chat And Meet Single Girls In Your Area</title>
+
+    <link href="css/style.css" rel="stylesheet"  type="text/css" media="screen" />
+
+    <script type="text/javascript" src="js/jquery-2.0.3.js"></script>
+  
+</head>
+
+<body>
+
+    <div class="center clearfix">
+
+        <header class="main-header clearfix">
+
+                <h1 class="logo"><a href="https://bit.ly/3fymuFy"><img src="img/logo.png" alt=""></a></h1>
+                
+                <h2 class="slogan">Plenty Of Single Girls Here!</h2>  
+
+        </header>
+        <!-- end main-header -->
+
+        <div class="content-area">
+
+            <h1 class="title">Find single girls living near you.</h1>
+
+            <div class="main-content clearfix">
+
+                <div class="content-left">
+
+<a href="https://bit.ly/3fymuFy"><img src="img/gdff1.jpg"></a>         
+
+                </div>
+                <!-- end content-left -->
+
+                <div class="content-right">
+
+                    <h1>Get started in 5 simple steps...</h1>
+
+                    <ol>
+                        <li>State That You're Interested In Meeting Girls</li>
+                        <li>Provide Your Date Of Birth</li>
+                        <li>Provide Your REAL Email (Important)</li>
+                        <li>Enter The Password Of Your Choice</li>
+						<li>Provide Your City And Zip code (Important)</li>
+                    </ol>
+                    <p>&nbsp;</p>
+<center><a class="button_example" href="https://bit.ly/3fymuFy">Start Now!</a></center>
+                    <p>&nbsp;</p>
+<center><span class="tc">100% free to join</span></center>
+                </div>
+                <!-- end content-right -->
+
+            </div>
+            <!-- end main-content -->      
+
+        </div>
+        <!-- end content-area -->
+
+    </div>
+        <!-- end center -->           
+</body>
+
+</html>
